23e95d6e252343d1fcaa127cbe1a420cf9055981
sijie <husijie19@outlook.com>
2024-11-03 17:39:27 +0900

    update

--- configs/deploy/detection_onnxruntime_static_mm.py
+++ configs/deploy/detection_onnxruntime_static_mm.py
@@ -1,28 +1,37 @@
 _base_ = ['./base_static.py']
+
+#########################################################################
+partition_flag = True  # if you want export part of the model, set to True, otherwise set to False to export the end2end model
+end_flag = 'yolo_head'  # output the cls_score, bbox_pred and objectness at three different scales
+# end_flag = 'yolo_head_raw'  # output the raw cls_score, bbox_pred before nms
+#########################################################################
+
 onnx_config = dict(
     input_names=['rgb', 'disp'],
     opset_version=11,)
 
 # If you want to export part of the model, you can use partition_config to indicate the start and end of the model.
-partition_config = dict(
-    type='single_stage_mm',
-    # Note: do not set the `apply_marks` in both `partition_config` and `codebase_config`, the
-    # `apply_marks` in `codebase_config` will be ignored.
-    apply_marks=True,  # set to True to apply marks for partition
-    data_preprocessor=dict(
-        type='DetDataPreprocessor_Disparity',
-        pad_size_divisor=32,
-        batch_augments=None
-        ),
-    partition_cfg=[
-        dict(
-            save_file='yolox.onnx',
-            # The start node of the model, which is pre-defined in the model (mmyolo/deploy/models/detectors/yolo_detector_mm.py)
-            start=['detector_forward:input'],
-            # The end node of the model, which is pre-defined in the model (mmyolo/deploy/models/dense_heads/yolov5_head.py)
-            end=['yolo_head_raw:input'],  # get scores and bboxes before nms
-            output_names=[f'pred_maps.{i}' for i in range(3)])
-    ])
+if partition_flag:
+    partition_config = dict(
+        type='single_stage_mm',
+        # Note: do not set the `apply_marks` in both `partition_config` and `codebase_config`, the
+        # `apply_marks` in `codebase_config` will be ignored.
+        apply_marks=True,  # set to True to apply marks for partition
+        data_preprocessor=dict(
+            type='DetDataPreprocessor_Disparity',
+            pad_size_divisor=32,
+            batch_augments=None
+            ),
+        partition_cfg=[
+            dict(
+                save_file='yolox.onnx',
+                # The start node of the model, which is pre-defined in the model (mmyolo/deploy/models/detectors/yolo_detector_mm.py)
+                start=['detector_forward:input'],
+                # The end node of the model, which is pre-defined in the model (mmyolo/deploy/models/dense_heads/yolov5_head.py)
+                end=[f'{end_flag}:input'],  # get scores and bboxes before nms
+                # output_names=[f'pred_maps.{i}' for i in range(3)]
+                )
+        ])
 codebase_config = dict(
     type='mmyolo',
     task='ObjectDetection_MM',
--- deploy_example/test_yolox_s_onnxruntime_rgb_depth.sh
+++ deploy_example/test_yolox_s_onnxruntime_rgb_depth.sh
@@ -1,17 +1,17 @@
-# To run this script, you need to first install mmdeploy (https://github.com/Superjie13/mmdeploy_multi_input) and mmyolo. Place the pretrained model checkpoint in the checkpoint folder.
-# mmdeploy, mmyolo, and the checkpoint folder should be in the same working directory.
-
-MMDEPLOY_DIR=./mmdeploy
-DEPLOY_CFG_PATH=./mmyolo/configs/deploy/detection_onnxruntime_static_mm.py
-MODEL_CFG_PATH=./mmyolo/configs/yolox/custom/yolox_s_mmyolo_airsim_drone_disp.py
-MODEL_CHECKPOINT_PATH=./checkpoint/rgbd_ocsort_detection.pth 
+# To run this script, you need to first install mmdeploy_multi_input (https://github.com/Superjie13/mmdeploy_multi_input) and mmyolo_rgbd. Place the pretrained model checkpoint in the checkpoint folder.
+# mmdeploy, mmyolo_rgbd, and the checkpoint folder should be in the same working directory.
+WORKSPACE=$(pwd)
+MMDEPLOY_DIR=${WORKSPACE}/mmdeploy_multi_input
+DEPLOY_CFG_PATH=${WORKSPACE}/mmyolo_rgbd/configs/deploy/detection_onnxruntime_static_mm.py
+MODEL_CFG_PATH=${WORKSPACE}/mmyolo_rgbd/configs/yolox/custom/yolox_s_mmyolo_airsim_drone_disp.py
+MODEL_CHECKPOINT_PATH=${WORKSPACE}/checkpoint/det_model.pth 
 
 python3 ${MMDEPLOY_DIR}/tools/deploy_mm.py \
     ${DEPLOY_CFG_PATH} \
     ${MODEL_CFG_PATH} \
     ${MODEL_CHECKPOINT_PATH} \
-    "./mmyolo/data/rgb_00000.png, ./mmyolo/data/disp_00000.png" \
-    --work-dir deploy_output \
+    "${WORKSPACE}/mmyolo_rgbd/data/rgb_00000.png, ${WORKSPACE}/mmyolo_rgbd/data/disp_00000.png" \
+    --work-dir deploy_output_2 \
     --device cpu \
     --log-level INFO \
     --show \
